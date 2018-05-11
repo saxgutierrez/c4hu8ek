b17bf9ce61321e3fbd6e6733b04e54cfd4db3ee0
saxgutierrez <diegofdog@gmail.com>
2018-05-11 16:53:10 -0500

    configuración de rutas de la API

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'pins#index'
 
-  devise_for :users
+  devise_for :users  #hubo que arreglar el error con la geme brypt
   resource :token, only: [:show]
   resources :pins, only: [:index, :new, :create]
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :pins, only: [:index, :create]
+      resources :pins, only: [:index, :create, :authenticate_user, :authenticate_token, :authenticate!] #agregando dos métodos de autenticación en la API
     end
   end
 end
